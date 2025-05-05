177c3106a8189003e9b16fb5b5a8f8f0e76ed639
Eternity-Sky <tengyumo666@hotmail.com>
2025-05-05 19:49:03 +0800

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -34,7 +34,7 @@ export default ({ mode }) =>
               },
             },
             {
-              urlPattern: /(.*?)\.(png|jpe?g|svg|gif|bmp|psd|tiff|tga|eps)/, // 图片缓存
+              urlPattern: /(.*?)\.(png|jpe?g|svg|gif|bmp|psd|tiff|tga|eps)/, // 图片缓存1
               handler: "CacheFirst",
               options: {
                 cacheName: "image-cache",
